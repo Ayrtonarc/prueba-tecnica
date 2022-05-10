03f7214bf051cd09f1f92f59e74815e135156ac7
Ayrtonarc <ayrton.arc@gmail.com>
2022-05-09 21:46:20 -0700

    mejora en la estructura del proyecto

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,4 +1,4 @@
-const { apiData, apiDataByTitle  } = require("./api");
+const { apiData, apiDataByTitle  } = require("../api");
 
 const resolvers = {
     Query: {
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const { ApolloServer} = require('apollo-server-express')
-const { typeDefs } = require('./typeDefs')
-const { resolvers } = require('./resolvers')
+const { typeDefs } = require('./graphql/typeDefs')
+const { resolvers } = require('./graphql/resolvers')
 
 const app = express()
 
